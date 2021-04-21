747ed9d02d2a1ed34208cb8d51dd72f0f53cbc53
AlexandruManafu <73789066+AlexandruManafu@users.noreply.github.com>
2021-04-21 11:47:27 +0300

    Todo List small improvements

--- todo.php
+++ todo.php
@@ -1,86 +1,54 @@
 <?php
-include('header.php');
+	include('header.php');
 ?>
-<link rel = "stylesheet" href="./style/todo.css">
-	<div class="wrapper">
+
+<div class="wrapper">
 	<div class="content">
-	<?php
-		require_once "scripts/functions.php";
-		require_once "scripts/database-handler.php";
-		
-		if(isset($_SESSION["usersId"]) && isset($_GET["project"]) )
-		{
-			$currentPage = basename(__FILE__);
-			$_SESSION["currentPage"] = $currentPage;
-			$inProjectFile = true;
-			$code = $_GET["project"];
-			$projectCode = getProjectCode($con,$code);
-			if( isOwnerProject($con,$_GET["project"],$_SESSION["usersName"]) )
-			{
-				$userRole = "manager";
-			}
-			else if( isDevInProject($con,$projectCode,$_SESSION["usersName"]) )
-			{
+    	<?php
+			require_once "scripts/functions.php";
+			require_once "scripts/database-handler.php";
+			if(isset($_SESSION["usersId"]) && isset($_GET["project"]) ){
+				$currentPage = basename(__FILE__);
+				$_SESSION["currentPage"] = $currentPage;
+				$inProjectFile = true;
 				$code = $_GET["project"];
 				$projectCode = getProjectCode($con,$code);
-				$userRole = "developer";
+				if( isOwnerProject($con,$_GET["project"],$_SESSION["usersName"]) ){
+					$userRole = "manager";
+				}
+				else if( isDevInProject($con,$projectCode,$_SESSION["usersName"]) ){
+					$code = $_GET["project"];
+					$projectCode = getProjectCode($con,$code);
+					$userRole = "developer";
+				}
+				else{
+					header("location: ./projects.php?error=projectNotJoined");
+					exit();
+				}
 			}
-			else
-			{
-				header("location: ./projects.php?error=projectNotJoined");
-				exit();
+			else{
+				header("location: ./index.php");
 			}
-		}
-		else
-		{
-			header("location: ./index.php");
-		}
-		
-		?>
-		
-		<div class = "rightDivContainingTable">
-            <table class="container">
-                <?php
-                    echo createTableHead(array("Title", "State", "Deadline", "Priority"));
-                    echo createTableBody($con,$_GET["project"]); 
-                    $urlPath = $_SERVER["REQUEST_URI"];
-					
-					
-                    ?>
-                
-            </table>
-        </div>
-            <div class = "leftDivContainingDescription"> 
-                <h1> Description </h1>
-                <hr>
-                <p id = "descriptionText"> </p>
-            </div>
-        </div>
-		
-		
-		
-		
-		<?php
-		
-		if(isset($_GET["error"]))
-		{
-			createIssueDisplay($currentPage,$code,$projectCode,$_GET["error"],$userRole);
-		}
-			else
-		{
-			createIssueDisplay($currentPage,$code,$projectCode,NULL,$userRole);
-		}
-				
-		require_once "projectIssueOptions.php";
-			
 		?>
 	
-	
+    	<table class="container">
+    	    <?php
+    	        echo createTableHead(array("Title", "Place", "Deadline", "Priority"));
+    	        echo createTableBody($con,$_GET["project"]); 
+    	        $urlPath = $_SERVER["REQUEST_URI"];
+    	        ?>
+    	</table>
+    	<?php
+			if(isset($_GET["error"])){
+				createIssueDisplay($currentPage,$code,$projectCode,$_GET["error"],$userRole);
+			}
+			else{
+				createIssueDisplay($currentPage,$code,$projectCode,NULL,$userRole);
+			}			
+		?>
+		<?php require_once("projectIssueOptions.php");?>
 	</div>
-	</div>
-	</body>
-	
-</html>
+</div>
 
 <?php 
     function createTableHead($params){
@@ -95,30 +63,28 @@ include('header.php');
 
     function createTableBody($con,$projectId){
 		$projectCode = getProjectCode($con,$_GET["project"]);
+		$priorities = array();
+		$priorities[0]="Abandoned";
+		$priorities[1]="Low";
+		$priorities[2]="Medium";
+		$priorities[3]="High";
 		
         $issues = listAllIssuesOfAProject($con, $projectCode); 
         $html = "<tbody>"; 
         while($row = mysqli_fetch_array($issues)){
-            $issueId = $row["issueId"]; //<a class='issueButton' href=".$_SESSION["currentPage"]."?project=".$projectId."&selectedIssue=".$issue["issueId"]>
+			$issueId = $row["issueId"];
 			$issueTitle = $row["issueTitle"];
 			$issuePlace = $row["issuePlace"];
             $issuePriority = $row["issuePriority"];
-            $issueStatus = $row["issueStatus"];
-            $details = $row["issueDetails"];
             $issueDeadline = $row["issueDeadline"];
-            $html .= "<tr><td onclick = 'displayDescription(\"$details\")'><a class='issueButton' href=".$_SESSION['currentPage']."?project=".$projectId."&selectedIssue=".$issueId.">".strval($issueTitle)."</a></td><td>". $issueStatus."</td><td>". $issueDeadline."</td><td>".$issuePriority."</td></tr>";
-           
+			if($issueDeadline=="0000-00-00")
+			{
+				$issueDeadline = "None";
+			}
+            $html .= "<tr><td><a class='issueButton' href=".$_SESSION['currentPage']."?project=".$projectId."&selectedIssue=".$issueId.">".strval($issueTitle)."</a></td><td>". $issuePlace."</td><td>". $issueDeadline."</td><td>".$priorities[$issuePriority]."</td></tr>";
         }
         $html .= "</tbody>";
         return $html; 
     }
 
-    ?>
-        <script>
-            function displayDescription(description){
-                document.getElementById("descriptionText").innerHTML = description;
-            }
-        </script>
-    <?php
-    require_once "projectIssueOptions.php";
 ?>
